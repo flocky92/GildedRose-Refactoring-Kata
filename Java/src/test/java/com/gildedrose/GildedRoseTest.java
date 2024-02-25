6b41121ded3a0a1a6ab4f1f518c8343090c2f815
flocky92 <161082563+flocky92@users.noreply.github.com>
2024-02-25 19:39:55 +0100

    add unit test legendary + backstage

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -86,13 +86,53 @@ class GildedRoseTest {
         assertEquals(4, app.getItems()[0].quality);
     }
 
-    // @Test
-    // void backstageProductSellInNegatif() {
-    //     Item[] items = new Item[] { new Item("Aged Brie", 0, 3) };
-    //     GildedRose app = new GildedRose(items);
-    //     app.updateProcess();
-    //     assertEquals("Aged Brie", app.getItems()[0].name);
-    //     assertEquals(-1, app.getItems()[0].sellIn);
-    //     assertEquals(5, app.getItems()[0].quality);
-    // }
+    @Test
+    void backstageProductSellInPositif10DaysLeft() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 3) };
+        GildedRose app = new GildedRose(items);
+        app.updateProcess();
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.getItems()[0].name);
+        assertEquals(9, app.getItems()[0].sellIn);
+        assertEquals(5, app.getItems()[0].quality);
+    }
+
+    @Test
+    void backstageProductSellInPositif5DaysLeft() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 3) };
+        GildedRose app = new GildedRose(items);
+        app.updateProcess();
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.getItems()[0].name);
+        assertEquals(4, app.getItems()[0].sellIn);
+        assertEquals(6, app.getItems()[0].quality);
+    }
+
+    @Test
+    void backstageProductSellInNegatif() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateProcess();
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.getItems()[0].name);
+        assertEquals(-1, app.getItems()[0].sellIn);
+        assertEquals(0, app.getItems()[0].quality);
+    }
+    
+    @Test
+    void legendaryProductSellInPositif() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 5, 82) };
+        GildedRose app = new GildedRose(items);
+        app.updateProcess();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.getItems()[0].name);
+        assertEquals(5, app.getItems()[0].sellIn);
+        assertEquals(82, app.getItems()[0].quality);
+    }
+
+    @Test
+    void legendaryProductSellInNegatif() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", -1, 75) };
+        GildedRose app = new GildedRose(items);
+        app.updateProcess();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.getItems()[0].name);
+        assertEquals(-1, app.getItems()[0].sellIn);
+        assertEquals(75, app.getItems()[0].quality);
+    }
 }
