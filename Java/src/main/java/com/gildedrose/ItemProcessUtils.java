740abf5ec0506b2d2c46d461c6d53a121a2bec35
flocky92 <161082563+flocky92@users.noreply.github.com>
2024-02-25 12:23:37 +0100

    Comment in code

--- Java/src/main/java/com/gildedrose/ItemProcessUtils.java
+++ Java/src/main/java/com/gildedrose/ItemProcessUtils.java
@@ -5,21 +5,33 @@ public final class ItemProcessUtils {
     final static int baseDegradationQuality = 1;
 
     private ItemProcessUtils(){}
-
+    /**
+     * Default process for commun product
+     * calculate degradation of product base of sellIn value * 
+     * @param item product item
+     * @param multiplicatorDegradation degree of degradation. 1 is normal
+     */
     public static void defaultQualityProcess(Item item, int multiplicatorDegradation){
         multiplicatorDegradation = multiplicatorDegradation <= 0 ? 1 : multiplicatorDegradation;
         int currentDregradation = baseDegradationQuality * multiplicatorDegradation;
         //negative sellIn -> X 2 degradation quality
         if(item.sellIn <= 0)
         currentDregradation *=2;
-
         item.quality = item.quality - currentDregradation;
     }
-
+    
+    /**
+     * Calculate quality for Aged Brie product
+     * @param item product item
+     */
     public static void agedBrieProcess(Item item){
         item.quality = item.sellIn > 0 ? item.quality + 1 : item.quality + 2;
     }
 
+    /**
+     * Calculate quality for backStage product
+     * @param item product item
+     */
     public static void backStageProcess(Item item){   
         if(item.sellIn <= 0)
             item.quality = 0;      
@@ -31,6 +43,11 @@ public final class ItemProcessUtils {
             item.quality ++;
     }
 
+    /**
+     * Decreases the sell in value 
+     * Check if quality is not negative
+     * @param item product item
+     */
     public static void checkDataValue(Item item){
         if(item.quality < 0)
             item.quality = 0;
